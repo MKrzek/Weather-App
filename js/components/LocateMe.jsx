04a73a527b4bd5387be2c64ac8ca122ed4ac370b
Malgorzata <mkrzek@googlemail.com>
2017-10-19 11:59:06 +0100

    added fetch for mylocation component

--- js/components/LocateMe.jsx
+++ js/components/LocateMe.jsx
@@ -5,22 +5,46 @@ constructor(props){
     super(props);
     this.state={
         latitude: null,
-        longitute: null, 
+        longitude: null, 
         error: null,
+        temperature: null,
+        description: null,
+        weatherIcon:null,
+        descriptionMain:null,
+        name: null,
+        render: false,
     }
 }
 componentDidMount(){
     this.watchId=navigator.geolocation.getCurrentPosition((position)=>{
         this.setState({
-            latitute:position.coords.latitude,
-            longitute: position.coords.longitude,
+            latitude:position.coords.latitude,
+            longitude: position.coords.longitude,
             error: null,
         });
+        this.handleMyLocationWeather()
     },
     (error)=>this.setState({ error: error.message}),
     {enableHighAccuracy:true, timeout: 30000, maximumAge: 1000},
-)
+    )    
+}
 
+handleMyLocationWeather=()=>{
+    let lat=this.state.latitude;
+    let lon=this.state.longitude;
+    return (fetch( `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&APPID=0d97dafb64ebaf36cf169cd4e5f02e5a`))
+    .then (r=>r.json())
+    .then (data=>{
+        console.log ('my location fetch')
+        this.setState({
+            temperature: data.main.temp,
+            description: data.weather[0].description,
+            weatherIcon:data.weather[0].icon,
+            descriptionMain:data.weather[0].main,
+            location: location.toUpperCase(),
+            name: data.name,
+        })
+    })
 }
     render(){
         return <div>
