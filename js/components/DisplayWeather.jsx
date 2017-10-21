ea49cc549fbeb0744ecd45ea1bbf1524a3be5fed
Malgorzata <mkrzek@googlemail.com>
2017-10-21 20:48:58 +0100

    added styling

--- js/components/DisplayWeather.jsx
+++ js/components/DisplayWeather.jsx
@@ -5,11 +5,10 @@ export default class DisplayWeather extends React.Component{
     render(){
         const {descriptionMain, description, temperature, weatherIcon, name} = this.props;
         return <div>
-                 <div>Current Weather</div>
-                <span>{name}</span>
-                <span>{temperature}°C</span>
-                <span>{descriptionMain}</span>
-                {weatherIcon?(<img src={'http://openweathermap.org/img/w/' + weatherIcon + '.png'} alt={description}/>): null}
+                 <h4>Current Weather in {name}</h4>
+                <span className='displayWeather-item'>{temperature}°C</span>
+                <span className='displayWeather-item' >{descriptionMain}</span>
+                {weatherIcon?(<img className='displayWeather-item'  src={'http://openweathermap.org/img/w/' + weatherIcon + '.png'} alt={description}/>): null}
                 
                
                </div>
