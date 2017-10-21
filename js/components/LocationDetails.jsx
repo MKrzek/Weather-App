5d8b2bd038b4dc813a9246f1783a31b4e48e1eaf
Malgorzata <mkrzek@googlemail.com>
2017-10-21 17:49:55 +0100

    added styling to locationitem/showlocation/locationdetail components

--- js/components/LocationDetails.jsx
+++ js/components/LocationDetails.jsx
@@ -6,10 +6,12 @@ export default class LocationDetails extends React.Component{
 render(){
     const{ location, name, descriptionMain, weatherIcon, description, temperature}=this.props;
     return <div>
-           <h3>{name}</h3>
-           <p> {temperature}°C</p>
-           <p> {descriptionMain}</p>
-            {weatherIcon? (<img src={'http://openweathermap.org/img/w/' + weatherIcon + '.png'} alt={description}/>): null}
+           <h3>Current weather in {name}</h3>
+           <div className='locationDetails'>
+                <p> {temperature}°C</p>
+                <p> {descriptionMain}</p>
+                {weatherIcon? (<img src={'http://openweathermap.org/img/w/' + weatherIcon + '.png'} alt={description}/>): null}
+           </div>
            </div>
 }
 }
